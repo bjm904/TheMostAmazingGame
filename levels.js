13a288185a45579c6acbf5fd18bff627cdb5bcce
Bryce Meyer <bjm904@gmail.com>
2013-11-03 00:00:42 -0700

    Create separate files for levels and maps

---
+++ levels.js
@@ -0,0 +1,16 @@
+//0-name,1-max turns,2-max players,3-mode,4-map ID,5-gridX,6-gridY,7-enemies,8-grid start x,9-grid start y
+//0-friendly name,1-type,2-inventory,3-x,4-y,5-remaining moves,6-Health,7-Strength,8-texture,9-level,10-Speed,11-has moved,12-deployed,13-has attacked,14-Defence
+var levels=[
+["Mammoth",5,4,0,0,20,20,[
+["Enemy 1",1,0,2,1,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]],
+["Enemy 2",1,0,9,9,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]],
+["Enemy 3",1,0,18,18,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]],
+["Enemy 4",1,0,18,2,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]],
+["Enemy 5",1,0,2,18,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]]
+],1,1],
+["City",8,2,0,1,20,20,[
+["Enemy 1",1,0,4,9,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]],
+["Enemy 2",1,0,17,11,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]],
+["Enemy 3",1,0,8,5,5,10,1,"enemy",1,1,0,1,0,1,"Arial",['fillerWeapon',1,'fillerWeapon',0,0,0,0,0,0,0],['fillerUpper',1,'fillerUpper',0,0,0,0,0,0,0],['fillerLower',1,'fillerLower',0,0,0,0,0,0,0]]
+]]
+];
